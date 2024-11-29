2bec056702ec9fb2bc4928f940c8e04fb521929d
Amit Chimya <amit.chimya@post.runi.ac.il>
2024-11-29 15:40:09 +0200

    Exercise 3 completed

--- MyString.java
+++ MyString.java
@@ -14,17 +14,45 @@ public class MyString {
         System.out.println(contains("personality", "son")); // true
         System.out.println(contains("personality", "dad")); // false
         System.out.println(contains("resignation", "sign")); // true
+
     }
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String newStr = "";
+        for (int i=0; i<str.length(); i++) {
+            if (str.charAt(i)>='A' && str.charAt(i)<='Z') {
+                newStr= newStr+ (char)(str.charAt(i)+32);
+            }
+            else newStr=newStr+str.charAt(i);
+        }
+
+        return newStr;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        if (str2=="") {
+            return true;
+        }
+        if (str1=="") {
+            return false;
+        }
+        for (int i=0; i<str1.length(); i++) {
+            if (str1.charAt(i)==str2.charAt(0)) {
+                boolean end = false;
+                for (int j=0; j<str2.length(); j++) {
+                    if (str1.charAt(i+j)==str2.charAt(j)) {
+                        end = true;
+                    }
+                    else {
+                        end = false;
+                        break;
+                    }
+                }
+                if (end==true) return true;
+            }
+        }
         return false;
     }
 }
